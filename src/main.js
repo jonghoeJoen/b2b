d40dae6875268f42634b4de66e18332171f0ce83
y <yeoungsul@gmail.com>
2022-06-27 15:57:30 +0900

    axios baseURL

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueClipboard from 'vue-clipboard2'
+import axios from "axios";
 
+axios.defaults.baseURL = 'http://127.0.0.1:5000'
+
+VueClipboard.config.autoSetContainer = true
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
+  VueClipboard,
+  axios,
   render: (h) => h(App),
 }).$mount("#app");
